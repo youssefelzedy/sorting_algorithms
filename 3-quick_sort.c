6b97b054789855bdd01a43456115140514925638
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-11-22 00:57:02 +0200

    Adding file 4

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,63 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * quick_sort_helper - Sorts an array of integers in ascending order using the
- *                     Quick sort algorithm.
- * @array: The array to be sorted.
- * @low: The lowest index of the partition to be sorted.
- * @high: The highest index of the partition to be sorted.
- * @size: The size of the array.
+ * swap - Swap two numbers in an array.
+ * @num1: The first number to swap.
+ * @num2: The second number to swap.
  */
-
-void quick_sort_helper(int *array, int low, int high, size_t size)
+void swap(int *num1, int *num2)
 {
-	int pivot, temp;
-	int i, j;
+	int tmp;
 
-	if (low < high)
-	{
-		pivot = low;
-		i = low;
-		j = high;
-
-		while (i < j)
-		{
-			while (array[i] <= array[pivot] && i <= high)
-				i++;
-			while (array[j] > array[pivot] && j >= low)
-				j--;
-			if (i < j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-		}
-
-		temp = array[j];
-		array[j] = array[pivot];
-		array[pivot] = temp;
-		print_array(array, size);
-
-		quick_sort_helper(array, low, j - 1, size);
-		quick_sort_helper(array, j + 1, high, size);
-	}
+	tmp = *num1;
+	*num1 = *num2;
+	*num2 = tmp;
 }
 
 /**
- * quick_sort - Sorts an array of integers in ascending order using the
- *              Quick sort algorithm.
- * @array: The array to be sorted.
- * @size: The size of the array.
+ * lomuto_partition - get partitions
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ * @low: Low index
+ * @high: high index
+ *
+ * Return: pivot
  */
+int lomuto_partition(int *array, size_t size, int low, int high)
+{
+	/* choose the last element as the pivot*/
+	int p, tmp_quick, j;
 
+	p = array[high]; /* last element is the pivot */
+	tmp_quick = low - 1; /* temporary pivot index */
+	j = low;
+
+	while (j < high)
+	{
+		if (array[j] < p)
+		{
+			tmp_quick++; /* move it forward */
+
+			if (tmp_quick != j)
+			{
+				swap(array + tmp_quick, array + j);
+				print_array(array, size);
+			}
+		}
+		j++;
+	}
+
+	if (array[high] < array[tmp_quick + 1])
+	{
+		swap(array + tmp_quick + 1, array + high);
+		print_array(array, size);
+	}
+
+	return (tmp_quick + 1);
+}
+
+/**
+ * quicksort - implement the quick sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ * @low: Low index
+ * @high: high index
+ *
+ * Return: Nothing
+ */
+void quicksort(int *array, size_t size, int low, int high)
+{
+	int pivot;
+
+	if (low >= high || low < 0)
+		return;
+
+	pivot = lomuto_partition(array, size, low, high);
+
+	/* sort partitions */
+	quicksort(array, size, low, pivot - 1); /* left side*/
+	quicksort(array, size, pivot + 1, high);
+}
+
+/**
+ * quick_sort - Sorts an array of numbers using quick sort
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ *
+ * Return: Nothing
+ */
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
+	{
 		return;
+	}
 
-	quick_sort_helper(array, 0, size - 1, size);
+	quicksort(array, size, 0, size - 1);
 }
