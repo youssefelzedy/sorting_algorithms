0f1d53e5d7757fb429e63886ea765a2fecf51eed
SaadSamir7 <saadsamir7177@gmail.com>
2023-11-22 00:38:00 +0200

    C - Sorting algorithms & Big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm.
+ * @list: Pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (list == NULL || *list == NULL)
+		return;
+
+	listint_t *current, *key, *temp;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		key = current;
+		while (key->prev != NULL && key->n < key->prev->n)
+		{
+			temp = key->prev;
+			key->prev = temp->prev;
+			temp->next = key->next;
+			key->next = temp;
+
+			if (temp->prev != NULL)
+				temp->prev->next = key;
+			else
+				*list = key;
+
+			temp->prev = key;
+
+			if (temp->next != NULL)
+				temp->next->prev = temp;
+
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
