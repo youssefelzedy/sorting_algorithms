b16a998d3368f302f7e0c3aa1e07c731c90ab6a1
SaadSamir7 <saadsamir7177@gmail.com>
2023-11-22 00:05:29 +0200

    C - Sorting algorithms & Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
+	size_t i, j;
+	int temp;
+	int swapped;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
 
-        for (j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
 
-                print_array(array, size);
-            }
-        }
+				print_array(array, size);
+			}
+		}
 
-        if (swapped == 0)
-            break;
-    }
+		if (swapped == 0)
+			break;
+	}
 }
